5e1240156d32b21f650a62ec7e933b8668281823
belka861 <83434319+belka861@users.noreply.github.com>
2021-05-08 14:08:50 -0700

    Update deployment.sh

--- deployment.sh
+++ deployment.sh
@@ -1,5 +1,9 @@
+#!/bin/sh
 sudo apt-get update -y
 sudo apt-get upgrade -y
 sudo apt-get install wget -y
 sudo apt-get install mc -y
 sudo apt-get install screen -y
+#sudo apt-get install xvf
+wget https://chromedriver.storage.googleapis.com/2.35/chromedriver_linux64.zip
+unzip chromedriver_linux64.zip
