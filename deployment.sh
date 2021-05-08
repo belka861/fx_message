e9525187497a7dcb4836a6c8faa8977de5d096e9
belka861 <83434319+belka861@users.noreply.github.com>
2021-05-08 14:18:34 -0700

    Update deployment.sh

--- deployment.sh
+++ deployment.sh
@@ -4,6 +4,8 @@ sudo apt-get upgrade -y
 sudo apt-get install wget -y
 sudo apt-get install mc -y
 sudo apt-get install screen -y
+sudo apt-get install python3-pip -y
 #sudo apt-get install xvf
 wget https://chromedriver.storage.googleapis.com/2.35/chromedriver_linux64.zip
 unzip chromedriver_linux64.zip
+pip3 install selenium
