4efc7a9c4d98673a23e15f0d0c14625e9facbf05
belka861 <83434319+belka861@users.noreply.github.com>
2021-05-08 15:47:17 -0700

    Update deployment.sh

--- deployment.sh
+++ deployment.sh
@@ -14,3 +14,4 @@ wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo apt install ./google-chrome-stable_current_amd64.deb -y
 pip3 install requests,transliterate
 wget https://datahub.io/core/nyse-other-listings/r/nyse-listed.csv
+pip3 install phone-gen
