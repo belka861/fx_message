18ca046e239c4bae94fd561d62ddffadf556bc68
belka861 <83434319+belka861@users.noreply.github.com>
2021-05-10 15:38:43 -0700

    Update deployment.sh

--- deployment.sh
+++ deployment.sh
@@ -5,11 +5,13 @@ sudo apt-get install wget -y
 sudo apt-get install mc -y
 sudo apt-get install screen -y
 sudo apt-get install python3-pip -y
-#sudo apt-get install xvf
+sudo apt-get install xvfb
+
 wget https://chromedriver.storage.googleapis.com/2.35/chromedriver_linux64.zip
 unzip chromedriver_linux64.zip
 
 wget https://raw.githubusercontent.com/belka861/fx_message/main/headless_test.py
+
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo apt install ./google-chrome-stable_current_amd64.deb -y
 
@@ -17,5 +19,5 @@ wget https://datahub.io/core/nyse-other-listings/r/nyse-listed.csv
 
 wget https://raw.githubusercontent.com/belka861/fx_message/main/h2.py
 wget https://raw.githubusercontent.com/belka861/fx_websocket/main/w5_edit.py
-pip3 install websocket-client
-pip3 install requests transliterate random_username phone-gen selenium websocket-client
+
+pip3 install requests transliterate random_username phone-gen selenium websocket-client pyvirtualdisplay
