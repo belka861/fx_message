78a6b69a325170fff9f575f5d97d22a73a19d651
belka861 <83434319+belka861@users.noreply.github.com>
2021-05-10 13:20:22 -0700

    Update deployment.sh

--- deployment.sh
+++ deployment.sh
@@ -8,14 +8,14 @@ sudo apt-get install python3-pip -y
 #sudo apt-get install xvf
 wget https://chromedriver.storage.googleapis.com/2.35/chromedriver_linux64.zip
 unzip chromedriver_linux64.zip
-pip3 install selenium
+
 wget https://raw.githubusercontent.com/belka861/fx_message/main/headless_test.py
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo apt install ./google-chrome-stable_current_amd64.deb -y
-pip3 install requests,transliterate
+
 wget https://datahub.io/core/nyse-other-listings/r/nyse-listed.csv
-pip3 install phone-gen
+
 wget https://raw.githubusercontent.com/belka861/fx_message/main/h2.py
 wget https://raw.githubusercontent.com/belka861/fx_websocket/main/w5_edit.py
 pip3 install websocket-client
-pip3 install requests transliterate random_username
+pip3 install requests transliterate random_username phone-gen selenium websocket-client
