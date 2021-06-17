d322dd82fd604386d475458f810ab813d9c3e077
belka861 <83434319+belka861@users.noreply.github.com>
2021-06-17 13:33:24 -0700

    no need click here

--- h2.py
+++ h2.py
@@ -787,7 +787,7 @@ def ptrend_chat_noreg():
     driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
     driver.save_screenshot("/test/screenshot.png")
 
-    _click('/html/body/div[2]/div/div[2]/section/form/div[1]/input')
+
 
 #    _wait_element('//*[@id="AskMeChatBot"]/div/div/img')
 #    _click('//*[@id="AskMeChatBot"]/div/div/img')
